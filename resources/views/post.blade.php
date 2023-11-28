3a9a0dbe71776930ddf3b500c821a88480c06931
monatanasov <simeon.g.atanasov@gmail.com>
2023-11-28 11:32:21 +0200

    Store blog posts as HTML files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,18 +5,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Tortor pretium viverra
-            suspendisse potenti nullam ac tortor vitae. Et egestas quis ipsum suspendisse. Purus viverra accumsan in nisl
-            nisi scelerisque eu ultrices. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo.
-            Lacus luctus accumsan tortor posuere ac. Amet tellus cras adipiscing enim eu. Dolor sit amet consectetur
-            adipiscing elit pellentesque. Nunc non blandit massa enim nec dui nunc. Id leo in vitae turpis massa sed
-            elementum tempus. Erat nam at lectus urna duis convallis convallis tellus. Leo urna molestie at elementum.
-            Molestie ac feugiat sed lectus vestibulum.
-        </p>
+        <?= $post; ?>
     </article>
 
     <a href="/">Go Back</a>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 
 <body>
 <article>
-    <h1><a href="/post">My First Post</a></h1>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
 
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
@@ -20,7 +20,7 @@
 </article>
 
 <article>
-    <h1><a href="/post">My Second Post</a></h1>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
 
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
@@ -35,7 +35,7 @@
 </article>
 
 <article>
-    <h1><a href="/post">My Third Post</a></h1>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
 
     <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore

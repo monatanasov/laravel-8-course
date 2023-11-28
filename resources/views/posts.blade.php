e03f56ca319c4531bc1201e30c6f90d6c13cf83f
monatanasov <simeon.g.atanasov@gmail.com>
2023-11-28 14:35:50 +0200

    Use the filesyste class to read a directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,48 +4,9 @@
 <link rel="stylesheet" href="app.css">
 
 <body>
-<article>
-    <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Tortor pretium viverra
-        suspendisse potenti nullam ac tortor vitae. Et egestas quis ipsum suspendisse. Purus viverra accumsan in nisl
-        nisi scelerisque eu ultrices. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo.
-        Lacus luctus accumsan tortor posuere ac. Amet tellus cras adipiscing enim eu. Dolor sit amet consectetur
-        adipiscing elit pellentesque. Nunc non blandit massa enim nec dui nunc. Id leo in vitae turpis massa sed
-        elementum tempus. Erat nam at lectus urna duis convallis convallis tellus. Leo urna molestie at elementum.
-        Molestie ac feugiat sed lectus vestibulum.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Tortor pretium viverra
-        suspendisse potenti nullam ac tortor vitae. Et egestas quis ipsum suspendisse. Purus viverra accumsan in nisl
-        nisi scelerisque eu ultrices. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo.
-        Lacus luctus accumsan tortor posuere ac. Amet tellus cras adipiscing enim eu. Dolor sit amet consectetur
-        adipiscing elit pellentesque. Nunc non blandit massa enim nec dui nunc. Id leo in vitae turpis massa sed
-        elementum tempus. Erat nam at lectus urna duis convallis convallis tellus. Leo urna molestie at elementum.
-        Molestie ac feugiat sed lectus vestibulum.
-    </p>
-</article>
-
-<article>
-    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Tortor pretium viverra
-        suspendisse potenti nullam ac tortor vitae. Et egestas quis ipsum suspendisse. Purus viverra accumsan in nisl
-        nisi scelerisque eu ultrices. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo.
-        Lacus luctus accumsan tortor posuere ac. Amet tellus cras adipiscing enim eu. Dolor sit amet consectetur
-        adipiscing elit pellentesque. Nunc non blandit massa enim nec dui nunc. Id leo in vitae turpis massa sed
-        elementum tempus. Erat nam at lectus urna duis convallis convallis tellus. Leo urna molestie at elementum.
-        Molestie ac feugiat sed lectus vestibulum.
-    </p>
-</article>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <?= $post; ?>
+        </article>
+    <?php endforeach; ?>
 </body>

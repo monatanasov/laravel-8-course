a22884449f8600d409cc301544446cd58c4c56e6
monatanasov <simeon.g.atanasov@gmail.com>
2023-11-30 12:22:11 +0200

    Your First Eloquent Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,9 +11,13 @@ class Post extends Model
 
     protected $guarded = ['id'];
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
     public function getRouteKeyName()
     {
         return 'slug';
     }
-
 }

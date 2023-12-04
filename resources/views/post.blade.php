d022a6cf92a2e0be85d1f076f27c7336d783fd36
monatanasov <simeon.g.atanasov@gmail.com>
2023-12-04 17:43:55 +0200

    Quick Tweaks and Clean-Up

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -43,7 +43,7 @@
                     <h1 class="font-bold text-3xl lg:text-4xl mb-10">
                         {{ $post->title }}
                     </h1>
-                    <div class="space-y-4 lg:text-lg leading-loose">{{ $post->body }}</div>
+                    <div class="space-y-4 lg:text-lg leading-loose">{!!  $post->body  !!}</div>
                 </div>
             </article>
         </main>

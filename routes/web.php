6c6c12c5309bc9fedbb5ac39912d3865a52edbbd
monatanasov <simeon.g.atanasov@gmail.com>
2023-12-06 14:10:22 +0200

    Build a Register User Page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -28,3 +29,6 @@ Route::get('authors/{author:username}', function (User $author) {
        'posts' => $author->posts
     ]);
 });
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);

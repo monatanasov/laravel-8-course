8c9883b607ae600c4b03691e2f433b56df7f2f46
monatanasov <simeon.g.atanasov@gmail.com>
2023-12-06 17:30:35 +0200

    Build the Log In Page

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,8 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        // validate the request
+        $attributes = request()->validate([
+           'email' => 'required|email',
+           'password' => 'required'
+        ]);
+
+        // attempt to authenticate and log in the user
+        // based on the provided credentials
+        if (auth()->attempt($attributes)) {
+            session()->regenerate(); //session fixation vulnerability
+            // redirect with a success flash message
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        //auth failed.
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+
+//        return back()
+//            ->withInput()
+//            ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+
+    }
+
     public function destroy()
     {
         auth()->logout();
--- routes/web.php
+++ routes/web.php
@@ -33,4 +33,8 @@ Route::get('authors/{author:username}', function (User $author) {
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
-Route::post('logout', [SessionsController::class, 'destroy']);
+
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');

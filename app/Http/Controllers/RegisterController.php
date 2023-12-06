df6fe0b9b8395af495d2b0afb571840402f51aa8
monatanasov <simeon.g.atanasov@gmail.com>
2023-12-06 14:31:50 +0200

    Automatic Password Hashing With Mutators

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -20,7 +20,8 @@ class RegisterController extends Controller
             'name' => 'required|max:255',
             'username' => 'required|max:255|min:3',
             'email' => 'required|email|max:255',
-            'password' => ['required', 'min:7', 'max:255']
+            'password' => 'required|min:7|max:255'
+            //'password' => ['required', 'min:7', 'max:255']
         ]);
 
         User::create($attributes);
--- app/Models/User.php
+++ app/Models/User.php
@@ -39,6 +39,11 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
     public function posts()
     {
         return $this->hasMany(Post::class);

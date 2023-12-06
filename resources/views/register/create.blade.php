55d7c1085184ee81b64c22ef452d5530aa2deaae
monatanasov <simeon.g.atanasov@gmail.com>
2023-12-06 15:12:12 +0200

    Failed Validation and Old Input Data

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -17,8 +17,15 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name') }}"
                            required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">
+                            {{ $message }}
+                        </p>
+                    @enderror
                 </div>
 
 
@@ -33,8 +40,14 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{ old('username') }}"
                            required
                     >
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">
+                        {{ $message }}
+                    </p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -48,8 +61,14 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email') }}"
                            required
                     >
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">
+                        {{ $message }}
+                    </p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -65,6 +84,11 @@
                            id="password"
                            required
                     >
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">
+                        {{ $message }}
+                    </p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -74,6 +98,15 @@
                         Submit
                     </button>
                 </div>
+
+
+{{--                @if($errors->any())--}}
+{{--                    <ul>--}}
+{{--                        @foreach($errors->all() as $error)--}}
+{{--                            <li class="text-red-500 text-xs">{{ $error }}</li>--}}
+{{--                        @endforeach--}}
+{{--                    </ul>--}}
+{{--                @endif--}}
 
             </form>
         </main>

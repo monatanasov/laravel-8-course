9a34f80532c5eefe87b0de7118ec2e164aa201ec
monatanasov <simeon.g.atanasov@gmail.com>
2023-12-07 17:18:18 +0200

    Make the Newsletter Form Work

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 |
 */
 
-Route::get('ping', function () {
-
+Route::post('newsletter', function () {
+    request()->validate(['email' => 'required|email']);
 
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
@@ -32,11 +32,19 @@ Route::get('ping', function () {
         'server' => 'us21'
     ]);
 
-    $response = $mailchimp->lists->addListMember('8f39f24f68', [
-        'email_address' => 'nemitrqa@abv.bg',
-        'status' => 'subscribed'
-    ]);
-    ddd($response);
+
+    try {
+        $response = $mailchimp->lists->addListMember('8f39f24f68', [
+            'email_address' => request('email'),
+            'status' => 'subscribed'
+        ]);
+    } catch (\Exception $e) {
+       throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list.'
+        ]);
+    }
+
+    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
 });
 
 Route::get('/', [PostController::class, 'index'])->name('home');

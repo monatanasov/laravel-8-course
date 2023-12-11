e6ae862a553fc7a33679b27e864ccd3c7511d236
monatanasov <simeon.g.atanasov@gmail.com>
2023-12-11 09:35:15 +0200

    Extract Form-Specific Blade Components

--- resources/views/posts/_add-comment-form.blade.php
+++ resources/views/posts/_add-comment-form.blade.php
@@ -12,21 +12,21 @@
                 <h2 class="ml-4">Want to participate?</h2>
             </header>
 
-            <div class="mt-6">
-                                    <textarea
-                                        name="body"
-                                        class="w-full text-sm focus:outline-none focus:ring"
-                                        rows="5"
-                                        placeholder="Quick, thing of something to say!"
-                                        required></textarea>
+            <x-form.field>
+                <textarea
+                    name="body"
+                    class="w-full text-sm focus:outline-none focus:ring"
+                    rows="5"
+                    placeholder="Quick, thing of something to say!"
+                    required></textarea>
 
                 @error('body')
                 <span class="text-xs text-red-500">{{ $message }}</span>
                 @enderror
-            </div>
+            </x-form.field>
 
             <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                <x-submit-button>Post</x-submit-button>
+                <x-form.button>Post</x-form.button>
             </div>
         </form>
     </x-panel>
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,110 +10,18 @@
             <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="title"
-                    >
-                        Title
-                    </label>
 
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           name="title"
-                           id="title"
-                           value="{{ old('title') }}"
-                           required
-                    >
+                <x-form.input name="title" />
+                <x-form.input name="slug" />
+                <x-form.input name="thumbnail" type="file" />
+                <x-form.textarea name="excerpt" />
+                <x-form.textarea name="body" />
 
-                    @error('title')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="slug"
-                    >
-                        Slug
-                    </label>
+                <x-form.field>
+                    <x-form.label name="category" />
 
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           name="slug"
-                           id="slug"
-                           value="{{ old('slug') }}"
-                           required
-                    >
-
-                    @error('slug')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="thumbnail"
-                    >
-                        Thumbnail
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="file"
-                           name="thumbnail"
-                           id="thumbnail"
-                           required
-                    >
-
-                    @error('thumbnail')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="excerpt"
-                    >
-                        Excerpt
-                    </label>
-
-                    <textarea class="border border-gray-400 p-2 w-full"
-                              type="text"
-                              name="excerpt"
-                              id="excerpt"
-                              required
-                    >{{ old('excerpt') }}</textarea>
-
-                    @error('excerpt')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="body"
-                    >
-                        Body
-                    </label>
-
-                    <textarea class="border border-gray-400 p-2 w-full"
-                              name="body"
-                              id="body"
-                              required
-                    >{{ old('body') }}</textarea>
-
-                    @error('body')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="category_id"
-                    >
-                        Category
-                    </label>
-
-                    <select name="category_id" id="category">
+                    <select name="category_id" id="category_id">
                         @php
                             $categories=\App\Models\Category::all();
                         @endphp
@@ -128,12 +36,10 @@
                         @endforeach
                     </select>
 
-                    @error('category')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-                </div>
+                    <x-form.error name="category"/>
+                </x-form.field>
 
-                <x-submit-button>Publish</x-submit-button>
+                <x-form.button>Publish</x-form.button>
 
             </form>
 
